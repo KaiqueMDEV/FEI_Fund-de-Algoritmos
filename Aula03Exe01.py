2c25c6988c33d71f108f6f177106393bfc1d8c3a
KaiqueMDEV <kaiquemedeirosmoreira@gmail.com>
2024-10-16 22:01:36 -0300

    Início do Projeto
    inicio do código de cadastro

--- Aula03Exe01.py
+++ Aula03Exe01.py
@@ -1,3 +1,5 @@
+#Algoritmo para a média e dizer se o aluno foi aprovado ou não, caso não, solicite a nota do exame e pergunte novamente, caso reprovado novamente, encerra código
+
 n1 = float(input("Informe a primeira nota: "))
 n2 = float(input("Informe a segunda nota: "))
 
